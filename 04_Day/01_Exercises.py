464109a1f45aa579e8f3f95d204ec162c2b34f98
adrianpelayo84 <adrian.pelayo@upa.edu.mx>
2025-03-03 10:48:12 -0600

    Avance de día 4

---
+++ 04_Day/01_Exercises.py
@@ -0,0 +1,24 @@
+# 1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'
+letter1 = 'Thirty'
+letter2 = 'Days'
+letter3 = 'Of'
+letter4 = 'Python'
+print(letter1 + ' ' + letter2 + ' ' + letter3 + ' ' + letter4)
+
+# 2. Concatenate the string 'Coding', 'For', 'All' to a single string, 'Coding For All'
+letter1 = 'Coding'
+letter2 = 'For'
+letter3 = 'All'
+print(letter1 + ' ' + letter2 + ' ' + letter3)
+
+# 3. Declare a variable named company and assign it to an initial value "Coding For All".
+company = 'Coding For All'
+
+# 4. Print the variable company using print()
+print(company)
+
+# 5. Print the length of the company string using len() method and print()
+print(len(company))
+
+# 6. Change all the characters to uppercase letters using upper() method
+print(company.upper())
